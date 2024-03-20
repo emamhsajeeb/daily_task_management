d05cddd07c7ec29a10d20caa2d8cf446488b4bd9
Emam Hosen <emamhsajeeb@gmail.com>
2024-03-20 21:20:10 +0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,11 +26,11 @@
                 <div class="col-lg-12">
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
-                            <a href="index.html" class="d-inline-block auth-logo">
-                                <img src="assets/images/logo-light.png" alt="" height="20">
+                            <a href="{{route('dashboard')}}" class="d-inline-block auth-logo">
+                                <img src="assets/images/logo.png" alt="" height="20">
                             </a>
                         </div>
-                        <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+                        <p class="mt-3 fs-15 fw-medium">Daily Task Management</p>
                     </div>
                 </div>
             </div>
@@ -43,7 +43,7 @@
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
                                 <h5 class="text-primary">Welcome Back !</h5>
-                                <p class="text-muted">Sign in to continue to Velzon.</p>
+                                <p class="text-muted">Sign in to continue</p>
                             </div>
                             <div class="p-2 mt-4">
                                 <form method="POST" action="{{ route('login') }}">
