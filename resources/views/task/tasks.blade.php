e1dafa076ababf6ed4891fcb705ccc20cef96585
Emam Hosen <emamhsajeeb@gmail.com>
2024-03-22 10:19:30 +0600

    Update tasks.blade.php

--- resources/views/task/tasks.blade.php
+++ resources/views/task/tasks.blade.php
@@ -6,6 +6,7 @@
     <div class="page-content">
         <div class="container-fluid">
             <!-- start page title -->
+@if($user->role == 'admin')
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
@@ -21,6 +22,7 @@
                     </div>
                 </div>
             </div>
+@endif
             <!-- end page title -->
 
 
