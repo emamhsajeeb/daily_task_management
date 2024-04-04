11cfd3b127ca03d8eafe9103f6e77a831b67141f
emamhsajeeb <emamhsajeeb@gmail.com>
2024-04-04 10:50:44 +0600

    Update service_worker.js

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,34 +1,7 @@
 console.log("This message is for the service worker");
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "pwabuilder-offline-page";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
-});
-
-self.addEventListener('install', async (event) => {
-    event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
-    );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: CACHE
-    })
-);
 
 self.addEventListener('push', function(event) {
     const data = event.data.json();
