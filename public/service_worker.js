cd32cc626607f84df77008d7ffe5311dc319f767
emamhsajeeb <emamhsajeeb@gmail.com>
2024-04-04 10:48:29 +0600

    Update service_worker.js

--- public/service_worker.js
+++ public/service_worker.js
@@ -32,28 +32,6 @@ workbox.routing.registerRoute(
     })
 );
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResp = await event.preloadResponse;
-
-                if (preloadResp) {
-                    return preloadResp;
-                }
-
-                const networkResp = await fetch(event.request);
-                return networkResp;
-            } catch (error) {
-
-                const cache = await caches.open(CACHE);
-                const cachedResp = await cache.match(offlineFallbackPage);
-                return cachedResp;
-            }
-        })());
-    }
-});
-
 self.addEventListener('push', function(event) {
     const data = event.data.json();
     event.waitUntil(
