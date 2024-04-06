a34da794cfeb29145c803fe858e1af2d940e2504
emamhsajeeb <emamhsajeeb@gmail.com>
2024-04-06 10:57:53 +0600

    ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\DailySummary;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,169 +18,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $roles = [
-            ['name' => 'manager', 'guard_name' => 'web'],
-            ['name' => 'admin', 'guard_name' => 'web'],
-            ['name' => 'visitor', 'guard_name' => 'web'],
-            ['name' => 'se', 'guard_name' => 'web'],
-            ['name' => 'qci', 'guard_name' => 'web'],
-            ['name' => 'aqci', 'guard_name' => 'web']
+
+        $dailySummary = [
+            ['date' => '2024-03-21', 'totalTasks' => 185, 'embankmentTasks' => 133, 'pavementTasks' => 8, 'structureTasks' => 44, 'totalResubmission' => 0],
+            ['date' => '2024-03-22', 'totalTasks' => 168, 'embankmentTasks' => 124, 'pavementTasks' => 8, 'structureTasks' => 36, 'totalResubmission' => 83],
+            ['date' => '2024-03-23', 'totalTasks' => 139, 'embankmentTasks' => 94, 'pavementTasks' => 5, 'structureTasks' => 40, 'totalResubmission' => 78],
+            ['date' => '2024-03-24', 'totalTasks' => 136, 'embankmentTasks' => 91, 'pavementTasks' => 5, 'structureTasks' => 40, 'totalResubmission' => 54],
+            ['date' => '2024-03-25', 'totalTasks' => 155, 'embankmentTasks' => 110, 'pavementTasks' => 3, 'structureTasks' => 42, 'totalResubmission' => 80],
+            ['date' => '2024-03-26', 'totalTasks' => 140, 'embankmentTasks' => 98, 'pavementTasks' => 5, 'structureTasks' => 37, 'totalResubmission' => 63],
+            ['date' => '2024-03-27', 'totalTasks' => 208, 'embankmentTasks' => 160, 'pavementTasks' => 5, 'structureTasks' => 43, 'totalResubmission' => 72],
+            ['date' => '2024-03-28', 'totalTasks' => 184, 'embankmentTasks' => 127, 'pavementTasks' => 9, 'structureTasks' => 48, 'totalResubmission' => 68],
+            ['date' => '2024-03-29', 'totalTasks' => 162, 'embankmentTasks' => 112, 'pavementTasks' => 10, 'structureTasks' => 40, 'totalResubmission' => 69],
+            ['date' => '2024-03-30', 'totalTasks' => 214, 'embankmentTasks' => 159, 'pavementTasks' => 8, 'structureTasks' => 47, 'totalResubmission' => 80],
+            ['date' => '2024-03-31', 'totalTasks' => 209, 'embankmentTasks' => 151, 'pavementTasks' => 11, 'structureTasks' => 47, 'totalResubmission' => 136],
+            ['date' => '2024-04-01', 'totalTasks' => 189, 'embankmentTasks' => 140, 'pavementTasks' => 12, 'structureTasks' => 37, 'totalResubmission' => 121],
+            ['date' => '2024-04-02', 'totalTasks' => 206, 'embankmentTasks' => 146, 'pavementTasks' => 13, 'structureTasks' => 47, 'totalResubmission' => 116],
+            ['date' => '2024-04-03', 'totalTasks' => 174, 'embankmentTasks' => 118, 'pavementTasks' => 9, 'structureTasks' => 47, 'totalResubmission' => 76],
+            ['date' => '2024-04-04', 'totalTasks' => 200, 'embankmentTasks' => 149, 'pavementTasks' => 9, 'structureTasks' => 42, 'totalResubmission' => 64]
         ];
 
-        foreach ($roles as $roleData) {
-            Role::create($roleData);
-        }
-
-        $permissions = [
-            ['name' => 'showTasksSE', 'guard_name' => 'web'],
-            ['name' => 'showTasks', 'guard_name' => 'web'],
-            ['name' => 'addTaskSE', 'guard_name' => 'web'],
-            ['name' => 'addTask', 'guard_name' => 'web'],
-            ['name' => 'importTasks', 'guard_name' => 'web'],
-            ['name' => 'exportTasks', 'guard_name' => 'web'],
-            ['name' => 'importCSV', 'guard_name' => 'web'],
-            ['name' => 'updateRfiSubmissionDate', 'guard_name' => 'web'],
-            ['name' => 'editProfile', 'guard_name' => 'web'],
-            ['name' => 'updateProfile', 'guard_name' => 'web'],
-            ['name' => 'deleteUser', 'guard_name' => 'web'],
-            ['name' => 'updateTaskStatus', 'guard_name' => 'web'],
-            ['name' => 'updateInspectionDetails', 'guard_name' => 'web'],
-            ['name' => 'updateCompletionDateTime', 'guard_name' => 'web']
-        ];
-
-        foreach ($permissions as $permissionData) {
-            Permission::create($permissionData);
-        }
-
-        $managerRole = Role::where('name', 'manager')->first();
-        $managerPermissions = ['showTasks','exportTasks'];
-        // Assign permissions to the 'manager' role
-        foreach ($managerPermissions as $permissionName) {
-            $permission = Permission::where('name', $permissionName)->first();
-            $managerRole->givePermissionTo($permission);
-        }
-        $adminRole = Role::where('name', 'admin')->first();
-        $adminPermissions = ['importTasks', 'addTask', 'exportTasks', 'importCSV', 'updateRfiSubmissionDate', 'editProfile', 'updateProfile', 'deleteUser', 'showTasks','updateTaskStatus', 'updateInspectionDetails', 'updateCompletionDateTime'];
-        // Assign permissions to the 'admin' role
-        foreach ($adminPermissions as $permissionName) {
-            $permission = Permission::where('name', $permissionName)->first();
-            $adminRole->givePermissionTo($permission);
-        }
-        $seRole = Role::where('name', 'se')->first();
-        $sePermissions = ['showTasksSE','addTaskSE','updateTaskStatus', 'updateInspectionDetails', 'updateCompletionDateTime'];
-        // Assign permissions to the 'admin' role
-        foreach ($sePermissions as $permissionName) {
-            $permission = Permission::where('name', $permissionName)->first();
-            $seRole->givePermissionTo($permission);
-        }
-
-        $admins = [
-            [
-                'employee_id' => '123',
-                'user_name' => 'abulbashar',
-                'first_name' => 'Md. Abul',
-                'last_name' => 'Bashar',
-                'position' => 'QC Manager',
-                'address' => 'Employee Address',
-                'phone' => '01465656565',
-                'passport' => 'A6541654',
-                'nid' => '9846541654',
-                'email' => 'abasharlged@gmail.com',
-                'password' => Hash::make(123456789),
-            ],
-            [
-                'employee_id' => '123',
-                'user_name' => 'emamhosen',
-                'first_name' => 'Emam',
-                'last_name' => 'Hosen',
-                'position' => 'IT Executive',
-                'address' => 'Employee Address',
-                'phone' => '01610285004',
-                'passport' => 'A6541654',
-                'nid' => '9846541654',
-                'email' => 'emamhsajeeb@gmail.com',
-                'password' => Hash::make(123456789),
-            ]
-        ];
-
-
-        foreach ($admins as $admin) {
-            $created = User::create($admin);
-            $created->assignRole('admin');
-        }
-
-        $ses = [
-            [
-                'employee_id' => '124',
-                'user_name' => 'debashis',
-                'first_name' => 'Debashis',
-                'last_name' => 'Jha',
-                'position' => 'Supervision Engineer',
-                'address' => 'Employee Address',
-                'phone' => '654654654564',
-                'passport' => 'A6541654',
-                'nid' => '9846541654',
-                'email' => 'debajha73@gmail.com',
-                'password' => Hash::make(123456789),
-            ],
-            [
-                'employee_id' => '126',
-                'user_name' => 'prodip',
-                'first_name' => 'Prodip',
-                'last_name' => 'Saha',
-                'address' => 'Employee Address',
-                'phone' => '657498435121',
-                'passport' => 'A6541654',
-                'nid' => '9846541654',
-                'position' => 'Supervision Engineer',
-                'email' => 'prodip678@gmail.com',
-                'password' => Hash::make(123456789),
-            ],
-            [
-                'employee_id' => '126',
-                'user_name' => 'rabbi',
-                'first_name' => 'Fozly',
-                'last_name' => 'Rabbi',
-                'address' => 'Employee Address',
-                'phone' => '98765413651',
-                'passport' => '727857',
-                'nid' => '785785785',
-                'position' => 'Supervision Engineer',
-                'email' => 'fozly.rabbi@dhakabypass.com',
-                'password' => Hash::make(123456789),
-            ],
-            [
-                'employee_id' => '126',
-                'user_name' => 'habibur',
-                'first_name' => 'Habibur',
-                'last_name' => 'Rahman',
-                'address' => 'Employee Address',
-                'phone' => '98465135156',
-                'passport' => 'A654178578654',
-                'nid' => '7457857',
-                'position' => 'Supervision Engineer',
-                'email' => 'habibur.rahman@dhakabypass.com',
-                'password' => Hash::make(123456789),
-            ],
-            // Add more users as needed
-        ];
-
-
-
-        foreach ($ses as $se) {
-            $created = User::create($se);
-            $created->assignRole('se');
-        }
-
-        $numberOfAuthors = 6;
-
-        // Loop to insert records with IDs from 1 to $numberOfAuthors
-        for ($i = 1; $i <= $numberOfAuthors; $i++) {
-            DB::table('authors')->insert([
-                'author_id' => $i,
-                // Add other columns and their values if needed
-                // For example: 'name' => 'Author ' . $i,
-            ]);
+        foreach ($dailySummary as $summary) {
+            DailySummary::create($summary);
         }
     }
 }
