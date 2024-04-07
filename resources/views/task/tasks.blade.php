241af765de327b410c71374b899e41706915091b
emamhsajeeb <emamhsajeeb@gmail.com>
2024-04-07 13:25:22 +0600

    Update tasks.blade.php

--- resources/views/task/tasks.blade.php
+++ resources/views/task/tasks.blade.php
@@ -378,8 +378,8 @@ async function updateTaskList() {
 
             await updateTaskListBody(tasks);
             $('#dateRangePicker').daterangepicker({
-                minDate: new Date(firstDate),
-                maxDate: new Date(lastDate),
+                minDate: firstDate,
+                maxDate: lastDate,
             }, function(start, end, label) {
                 console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
             });
