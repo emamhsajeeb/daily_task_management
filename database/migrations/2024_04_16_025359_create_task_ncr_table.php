77be0f23c32b0133ed0128e4bf3d64e7eed8d264
Emam Hosen <emamhsajeeb@gmail.com>
2024-04-20 00:11:58 +0600

    Update 2024_04_16_025359_create_task_ncr_table.php

--- database/migrations/2024_04_16_025359_create_task_ncr_table.php
+++ database/migrations/2024_04_16_025359_create_task_ncr_table.php
@@ -11,17 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('task_has_ncr', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('task_id');
-            $table->unsignedBigInteger('ncr_id');
-            // Add additional columns if needed
-            $table->timestamps();
 
-            // Define foreign key constraints
-            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
-            $table->foreign('ncr_id')->references('id')->on('n_c_r_s')->onDelete('cascade');
-        });
     }
 
     /**
