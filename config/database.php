886b7150e12c355ca657fd72e0f33e7318dd701a
Emam Hosen <emamhsajeeb@gmail.com>
2024-04-21 23:14:53 +0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'max_connections' => env('DB_MAX_CONNECTIONS', 10)
         ],
 
         'pgsql' => [
