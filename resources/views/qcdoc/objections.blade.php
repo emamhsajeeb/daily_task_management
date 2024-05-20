9b955feb6093cd21ed41617e3a13e8c8484adf74
emamhsajeeb <emamhsajeeb@gmail.com>
2024-05-20 12:21:33 +0600

    Update objections.blade.php

--- resources/views/qcdoc/objections.blade.php
+++ resources/views/qcdoc/objections.blade.php
@@ -135,108 +135,39 @@
     </div>
     <!--end modal-->
 
-    <div class="modal fade" id="exampleModalScrollable" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+    <div class="modal modal-lg zoomIn" id="ncrDetailsModal" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalScrollableTitle">Scrollable
-                        Modal</h5>
+                    <h5 class="modal-title" id="ncrNumber"></h5><h5 class="modal-title" id="ncrDate"></h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                     </button>
                 </div>
-                <div class="modal-body">
-                    <h6 class="fs-15">Give your text a good structure</h6>
-                    <div class="d-flex">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2">
-                            <p class="text-muted mb-0">Raw denim you probably haven't heard of them jean shorts Austin.
-                                Nesciunt tofu stumptown aliqua, retro synth master cleanse.</p>
-                        </div>
-                    </div>
+                <div  class="modal-body">
+                    <h6 id="ncrDate" class="fs-15">Details:</h6>
                     <div class="d-flex mt-2">
                         <div class="flex-shrink-0">
                             <i class="ri-checkbox-circle-fill text-success"></i>
                         </div>
                         <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">Too much or too little spacing, as in the example below, can make things unpleasant for the reader. The goal is to make your text as comfortable to read as possible. </p>
+                            <p id="ncrDetails" class="text-muted mb-0"></p>
                         </div>
                     </div>
+                    <h6 id="ncrDate" class="fs-15">Chainages:</h6>
                     <div class="d-flex mt-2">
                         <div class="flex-shrink-0">
                             <i class="ri-checkbox-circle-fill text-success"></i>
                         </div>
                         <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">In some designs, you might adjust your tracking to create a certain artistic effect. It can also help you fix fonts that are poorly spaced to begin with.</p>
+                            <p id="ncrChainages" class="text-muted mb-0"></p>
                         </div>
                     </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">For that very reason, I went on a quest and spoke to many different professional graphic designers and asked them what graphic design tips they live.</p>
-                        </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
                     </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">You've probably heard that opposites attract. The same is true for fonts. Don't be afraid to combine font styles that are different but complementary, like sans serif with serif, short with tall, or decorative with simple. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">For that very reason, I went on a quest and spoke to many different professional graphic designers and asked them what graphic design tips they live.</p>
-                        </div>
-                    </div>
-                    <h6 class="fs-16 my-3">Graphic Design</h6>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">Opposites attract, and that’s a fact. It’s in our nature to be interested in the unusual, and that’s why using contrasting colors in Graphic Design is a must. It’s eye-catching, it makes a statement, it’s impressive graphic design. Increase or decrease the letter spacing depending.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">Trust fund seitan letterpress, keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">Just like in the image where we talked about using multiple fonts, you can see that the background in this graphic design is blurred. Whenever you put text on top of an image, it’s important that your viewers can understand.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mt-2">
-                        <div class="flex-shrink-0">
-                            <i class="ri-checkbox-circle-fill text-success"></i>
-                        </div>
-                        <div class="flex-grow-1 ms-2 ">
-                            <p class="text-muted mb-0">Keytar raw denim keffiyeh etsy art party before they sold out master cleanse gluten-free squid scenester freegan cosby sweater.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
-
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
 
     <script>
         const admin = {{$user->hasRole('admin') ? 'true' : 'false'}};
@@ -296,7 +227,7 @@
                     <td>${ iconHtml + statusOptions}</td>
                     <td>${objection.remarks ? objection.remarks : "N/A" }</td>
                     <td>
-                        <button type="button" obj-id=${objection.id} id="objectionDetails" class="btn btn-sm btn-light">Details</button>
+                        <button type="button" obj_id=${objection.id} id="objectionDetails" class="btn btn-sm btn-light objection-details-btn">Details</button>
                     </td>
                 </tr>
             `;
@@ -417,6 +348,15 @@
         {{--        }--}}
         {{--    })--}}
         {{--}--}}
+        // Function to fetch and display NCR details
+        async function objectionDetails(objection) {
+            // Update modal body with NCR details
+            $('#ncrDetailsModal').find('#ncrNumber').text(objection.obj_no);
+            // Apply inline CSS
+            $('#ncrDetailsModal').find('#ncrDetails').html('<pre style="font-family: Arial, sans-serif; font-size: 14px; color: #333; text-wrap: wrap;">' + objection.details + '</pre>');
+            $('#ncrDetailsModal').find('#ncrChainages').html('<pre style="font-family: Arial, sans-serif; font-size: 14px; color: #333; text-wrap: wrap;">' + objection.chainages + '</pre>');
+            $("#ncrDetailsModal").modal('show');
+        }
 
         $( document ).ready(async function () {
             $.ajaxSetup({
@@ -435,6 +375,12 @@
                 $(this).html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Adding...');
                 $(this).prop('disabled', true);
                 await addObjection();
+            });
+
+            $(document).on('click', '.obection-details-btn', async function () {
+                const objectionId = parseInt($(this).attr('obj_id'));
+                const objection = objections.find(item => item.id === objectionId);
+                await objectionDetails(objection);
             });
 
             $('#filterTasks').click(async function (e) {
