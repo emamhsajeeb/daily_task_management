f1b7151d2b71d713cc73f6f6a8cf323cf529dc28
Emam Hosen <emamhsajeeb@gmail.com>
2024-05-25 13:23:13 +0600

    Update tasks.blade.php

--- resources/views/task/tasks.blade.php
+++ resources/views/task/tasks.blade.php
@@ -336,7 +336,7 @@ async function updateTaskListBody(tasks, incharges, juniors) {
                     if (type === 'display' && data.length > 30) {
                         return `<span style="overflow-y: auto; max-height: 30px;" title="${data}">
                         ${data.substr(0, 30)}
-                        <button tabindex="0" role="button" class="btn btn-light" data-bs-toggle="popover" data-bs-trigger="focus" title="Descriptions" data-bs-placement="top" data-bs-content='${data}'>
+                        <button tabindex="0" role="button" class="btn btn-light" data-toggle="popover" data-trigger="focus" title="Descriptions" data-placement="top" data-content='${data}'>
                             ...
                         </button>
                     </span>`;
