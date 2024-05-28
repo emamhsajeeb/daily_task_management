b304c7328b6d51bda523745801858cff7068d4e2
Emam Hosen <emamhsajeeb@gmail.com>
2024-05-28 15:56:21 +0600

    Update objections.blade.php

--- resources/views/qcdoc/objections.blade.php
+++ resources/views/qcdoc/objections.blade.php
@@ -374,14 +374,14 @@
                 // For example, you can use AJAX to send a request to the server to edit the task
                 // Replace the URL with your actual endpoint for editing a task
                 $.ajax({
-                    url: "{{ route('editObjection') }}",
+                    url: "{{ route('updateObjection') }}",
                     type: "POST",
                     data: {
                         id: objId
                     },
                     success: function(data) {
                         // Handle success response
-                        console.log("Objection edited successfully");
+                        console.log("Objection updated successfully");
                         // You can add further actions as needed after editing the task
                     },
                     error: function(xhr, status, error) {
