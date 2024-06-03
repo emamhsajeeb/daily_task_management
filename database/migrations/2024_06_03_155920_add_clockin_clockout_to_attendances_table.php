91ba5eab9fc18969129672e013e4f065639aa4c9
Emam Hosen <emamhsajeeb@gmail.com>
2024-06-03 16:08:59 +0600

    Update 2024_06_03_155920_add_clockin_clockout_to_attendances_table.php

--- database/migrations/2024_06_03_155920_add_clockin_clockout_to_attendances_table.php
+++ database/migrations/2024_06_03_155920_add_clockin_clockout_to_attendances_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddClockinClockoutLocationToAttendancesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class AddClockinClockoutLocationToAttendancesTable extends Migration
             $table->dropColumn(['clockin', 'clockout', 'clockin_location','clockout_location']);
         });
     }
-}
+};
