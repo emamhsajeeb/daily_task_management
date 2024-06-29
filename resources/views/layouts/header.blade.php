2d1ac6ed52369961874ca7377fe40d767277df1c
Emam Hosen <emamhsajeeb@gmail.com>
2024-06-29 10:34:36 +0600

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -24,7 +24,7 @@
                         </div>
 
                         <button type="button" class="btn btn-sm px-3 fs-16 header-item vertical-menu-btn topnav-hamburger" id="topnav-hamburger-icon">
-                            <span class="hamburger-icon">
+                            <span class="ri-menu-2-line">
                                 <span></span>
                                 <span></span>
                                 <span></span>
